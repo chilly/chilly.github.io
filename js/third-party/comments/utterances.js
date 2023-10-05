162c58963b266f61776a5fc59358020ab46c3767
chillyc <chillycreator@gmail.com>
2023-10-06 00:34:44 +0800

    Site updated: 2023-10-06 00:33:56

---
+++ js/third-party/comments/utterances.js
@@ -0,0 +1,18 @@
+/* global NexT, CONFIG */
+
+document.addEventListener('page:loaded', () => {
+  if (!CONFIG.page.comments) return;
+
+  NexT.utils.loadComments('.utterances-container')
+    .then(() => NexT.utils.getScript('https://utteranc.es/client.js', {
+      attributes: {
+        async       : true,
+        crossOrigin : 'anonymous',
+        'repo'      : CONFIG.utterances.repo,
+        'issue-term': CONFIG.utterances.issue_term,
+        'theme'     : CONFIG.utterances.theme,
+        'label'     : 'Comment'
+      },
+      parentNode: document.querySelector('.utterances-container')
+    }));
+});
